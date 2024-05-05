f2cd8295441eab60c6e5a83816eae5c9007664d5
Jordan Mackie <12185627+jmackie@users.noreply.github.com>
2024-05-05 12:53:05 +0200

    Rename modules -> home-manager-modules

--- home.nix
+++ home.nix
@@ -1,17 +1,17 @@
-{ pkgs, ... }: {
+{ ... }: {
   home.stateVersion = "23.11";
   imports = [
-    ./modules/eza
-    ./modules/gh
-    ./modules/git
-    ./modules/helix
-    ./modules/nnn
-    ./modules/starship
-    ./modules/tools
-    ./modules/usql
-    ./modules/wezterm
-    ./modules/zoxide
-    ./modules/zsh
+    ./home-manager-modules/eza
+    ./home-manager-modules/gh
+    ./home-manager-modules/git
+    ./home-manager-modules/helix
+    ./home-manager-modules/nnn
+    ./home-manager-modules/starship
+    ./home-manager-modules/tools
+    ./home-manager-modules/usql
+    ./home-manager-modules/wezterm
+    ./home-manager-modules/zoxide
+    ./home-manager-modules/zsh
   ];
   home.sessionVariables = {
     PAGER = "less -S";
