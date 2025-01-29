1d64e76af1eb886749b2cbdbf47f418410e35f7b
Jordan Mackie <12185627+jmackie@users.noreply.github.com>
2025-01-29 17:05:47 +0200

    Add smartcat 🐱

--- home-manager-modules/tools/default.nix
+++ home-manager-modules/tools/default.nix
@@ -10,6 +10,19 @@
     httpie
     ctop
     cachix
+
+    (rustPlatform.buildRustPackage rec {
+      pname = "smartcat";
+      version = "2.2.0";
+      src = fetchFromGitHub {
+        owner = "efugier";
+        repo = pname;
+        rev = version;
+        hash = "sha256-nXuMyHV5Sln3qWXIhIDdV0thSY4YbvzGqNWGIw4QLdM=";
+      };
+      useFetchCargoVendor = true;
+      cargoHash = "sha256-tR7+SecTS1FWwcPF25PclT6lEjY9NUEj/2EBhbgg0tw=";
+    })
   ];
   programs.bat.enable = true;
   programs.direnv = {
